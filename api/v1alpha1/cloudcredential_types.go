2300c03b37a67ee02a2db108be92f383fa5489cc
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-05-18 09:44:24 +0900

    aws만 되는 초기안

---
+++ api/v1alpha1/cloudcredential_types.go
@@ -0,0 +1,68 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CloudCredentialSpec defines the desired state of CloudCredential
+type CloudCredentialSpec struct {
+	Provider    string `json:"provider"`
+	AccessKeyID string `json:"accesskeyid"`
+	AccessKey   string `json:"accesskey"`
+
+	Region string `json:"region,omitempty"`
+}
+
+// CloudCredentialStatus defines the observed state of CloudCredential
+type CloudCredentialStatus struct {
+	// Message shows log when the status changed in last
+	Message string `json:"message,omitempty" protobuf:"bytes,2,opt,name=message"`
+	// Reason shows why the status changed in last
+	Reason string `json:"reason,omitempty" protobuf:"bytes,3,opt,name=reason"`
+	// LastTransitionTime shows the time when the status changed in last
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,3,opt,name=lastTransitionTime"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// CloudCredential is the Schema for the cloudcredentials API
+type CloudCredential struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CloudCredentialSpec   `json:"spec,omitempty"`
+	Status CloudCredentialStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// CloudCredentialList contains a list of CloudCredential
+type CloudCredentialList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CloudCredential `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CloudCredential{}, &CloudCredentialList{})
+}
---
+++ controllers/cloudcredential_controller.go
@@ -0,0 +1,100 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"strings"
+
+	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	credential "github.com/tmax-cloud/cloud-credential-operator/api/v1alpha1"
+)
+
+// CloudCredentialReconciler reconciles a CloudCredential object
+type CloudCredentialReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=credentials.tmax.io,resources=cloudcredentials,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=credentials.tmax.io,resources=cloudcredentials/status,verbs=get;update;patch
+
+func (r *CloudCredentialReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	log := r.Log
+	log.Info("Reconciling CloudCredential")
+
+	cloudCredential := &credential.CloudCredential{}
+	err := r.Get(context.TODO(), req.NamespacedName, cloudCredential)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("CloudCredential resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get CloudCredential")
+		return ctrl.Result{}, err
+	}
+
+	provider := cloudCredential.Spec.Provider
+
+	accessKeyID := cloudCredential.Spec.AccessKeyID
+	accessKey := cloudCredential.Spec.AccessKey
+	region := cloudCredential.Spec.Region
+
+	var data map[string]string
+	data = make(map[string]string)
+
+	if strings.EqualFold(provider, "aws") {
+		data["credentials"] = "[default]\n" + "aws_access_key_id = " + accessKeyID + "\n" + "aws_secret_access_key = " + accessKey + "\n"
+		data["config"] = "[default]\n" + "region = " + region + "\n" + "output = json" + "\n"
+	}
+
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        cloudCredential.Name,
+			Namespace:   cloudCredential.Namespace,
+			Annotations: cloudCredential.Annotations,
+		},
+		//Immutable: false,
+		Type:       v1.SecretTypeOpaque,
+		StringData: data,
+	}
+
+	err = r.Create(context.TODO(), secret)
+
+	if err != nil {
+		log.Error(err, "Failed to create Secret")
+		cloudCredential.Status.Message = err.Error()
+		cloudCredential.Status.Reason = "Failed to create Secret"
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *CloudCredentialReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&credential.CloudCredential{}).
+		Complete(r)
+}
