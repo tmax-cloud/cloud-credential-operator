09a38cda93c035d7ca5db2d8a208756bbff81182
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-12-21 17:36:37 +0900

    [mod] role apigroup,apiversion 수정

--- controllers/cloudcredential_controller.go
+++ controllers/cloudcredential_controller.go
@@ -75,7 +75,7 @@ var (
 // +kubebuilder:rbac:groups=credentials.tmax.io,resources=cloudcredentials,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=credentials.tmax.io,resources=cloudcredentials/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=core,resources=secrets;services,verbs=get;list;create;delete;patch;update
-// +kubebuilder:rbac:groups=rbac,resources=roles;rolebindings,verbs=get;list;create;delete;patch;update
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles;rolebindings,verbs=get;list;create;delete;patch;update
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;create;delete;patch;update
 
 func (r *CloudCredentialReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
